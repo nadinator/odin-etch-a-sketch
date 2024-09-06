281511adfc39e219850908aef4fe7830af69a658
nba <64587372+nadinator@users.noreply.github.com>
2024-09-06 08:57:02 +0300

    Finish basic EAS

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const container = document.querySelector(".container");
-
+const width = window.innerWidth / 2;
+const height = window.innerHeight / 2;
 let n = -1;
 
 while (n <= 0 || n > 100 || isNaN(n)) {
   n = prompt("Enter a number between 1 and 100");
 }
+
+let boxSize = Math.min(width, height) / n;
 
 // Row
 for (let i = 0; i < n; i++) {
@@ -14,8 +17,14 @@ for (let i = 0; i < n; i++) {
   row.style.justifyContent = "space-between";
   for (let j = 0; j < n; j++) {
     const box = document.createElement("div");
-    box.style.border = "solid 10px red";
-    box.style.margin = "5px";
+    box.style.width = boxSize + "px";
+    box.style.height = boxSize + "px";
+    box.style.border = "solid 1px red";
+    box.style.margin = "2px";
+    box.style.boxSizing = "border-box";
+    box.addEventListener("mouseover", () => {
+      box.style.backgroundColor = "red";
+    });
     row.appendChild(box);
   }
   container.appendChild(row);
