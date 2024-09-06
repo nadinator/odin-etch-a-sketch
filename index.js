739f2f0508d0dc7a324ea550a81d04227c7d7a9c
nba <64587372+nadinator@users.noreply.github.com>
2024-09-06 09:20:53 +0300

    Add reset button

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
 const container = document.querySelector(".container");
 const width = window.innerWidth / 2;
 const height = window.innerHeight / 2;
-let n = -1;
 
-while (n <= 0 || n > 100 || isNaN(n)) {
-  n = prompt("Enter a number between 1 and 100");
-}
-
-let boxSize = Math.min(width, height) / n;
-
-// Row
-for (let i = 0; i < n; i++) {
-  // Column
-  const row = document.createElement("div");
-  row.style.display = "flex";
-  row.style.justifyContent = "space-between";
-  for (let j = 0; j < n; j++) {
-    const box = document.createElement("div");
-    box.style.width = boxSize + "px";
-    box.style.height = boxSize + "px";
-    box.style.border = "solid 1px red";
-    box.style.margin = "2px";
-    box.style.boxSizing = "border-box";
-    box.addEventListener("mouseover", () => {
-      box.style.backgroundColor = "red";
-    });
-    row.appendChild(box);
+function createGrid() {
+  let n = -1;
+  while (n <= 0 || n > 100 || isNaN(n)) {
+    n = prompt("Enter a number between 1 and 100");
   }
-  container.appendChild(row);
+
+  let boxSize = Math.min(width, height) / n;
+
+  for (let i = 0; i < n; i++) {
+    const row = document.createElement("div");
+    row.style.display = "flex";
+    row.style.justifyContent = "space-between";
+    for (let j = 0; j < n; j++) {
+      const box = document.createElement("div");
+      box.style.width = boxSize + "px";
+      box.style.height = boxSize + "px";
+      box.style.border = "solid 1px red";
+      box.style.margin = "1px";
+      box.style.boxSizing = "border-box";
+      box.addEventListener("mouseover", () => {
+        box.style.backgroundColor = "red";
+      });
+      row.appendChild(box);
+    }
+    container.appendChild(row);
+  }
 }
+
+const resetButton = document.querySelector("#resetButton");
+resetButton.addEventListener("click", () => {
+  Array.from(container.children)
+    .filter((child) => child !== resetButton)
+    .forEach((child) => container.removeChild(child));
+  createGrid();
+});
+
+createGrid();
